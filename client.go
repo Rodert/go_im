8232f336f20b19c78b8e332e831e5675e5817da5
wsy <wangshiyu@ai-finance.cn>
2022-07-28 16:21:07 +0800

    client

--- client.go
+++ client.go
@@ -3,7 +3,9 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io"
 	"net"
+	"os"
 )
 
 type Client struct {
@@ -38,6 +40,109 @@ func (client *Client) menu() bool {
 
 	fmt.Scanln(&flag)
 
+	if flag >= 0 && flag <= 3 {
+		client.flag = flag
+		return true
+	} else {
+		fmt.Println("请输入合法数据范围")
+		return false
+	}
+}
+
+func (client *Client) Run() {
+	for client.flag != 0 {
+		for !client.menu() {
+		}
+
+		switch client.flag {
+		case 1:
+			fmt.Println("公聊模式")
+		case 2:
+			fmt.Println("私聊模式")
+		case 3:
+			fmt.Println("g更新用户名")
+		}
+	}
+	fmt.Println("退出")
+}
+
+func (client *Client) updateName() bool {
+	fmt.Println("》》》请输入用户名")
+	fmt.Scanln(&client.Name)
+
+	sendMsg := "rename|" + client.Name + "\n"
+	_, err := client.conn.Write([]byte(sendMsg))
+
+	if err != nil {
+		fmt.Println("conn.Write err:", err)
+		return false
+	}
+	return true
+}
+
+func (client *Client) DealResponse() {
+	io.Copy(os.Stdout, client.conn)
+}
+
+func (client *Client) publicChat() {
+	var chatMsg string
+
+	fmt.Println(">>>>请输入聊天内容，exit退出")
+	fmt.Scanln(&chatMsg)
+
+	for chatMsg != "eixt" {
+		if len(chatMsg) != 0 {
+			sendMsg := chatMsg + "\n"
+			_, err := client.conn.Write([]byte(sendMsg))
+			if err != nil {
+				fmt.Println("conn Write err:", err)
+				break
+			}
+		}
+		chatMsg = ""
+		fmt.Println(">>>>请输入聊天内容，exit退出")
+		fmt.Scanln(&chatMsg)
+	}
+}
+
+func (clinet *Client) SelectUsers() {
+	sendMsg := "who\n"
+	_, err := clinet.conn.Write([]byte(sendMsg))
+	if err != nil {
+		fmt.Println("conn White err:", err)
+		return
+	}
+}
+
+func (client *Client) PrivateChat() {
+	var remoteName string
+	var chatMsg string
+
+	client.SelectUsers()
+	fmt.Println(">>>>>>请输入聊天对象的[用户名]，exit退出")
+	fmt.Scanln(&remoteName)
+
+	for remoteName != "exit" {
+		fmt.Println("请输入消息内容，exit退出")
+		fmt.Scanln(&chatMsg)
+
+		for chatMsg != "exit" {
+			if len(chatMsg) != 0 {
+				sendMsg := "to|" + remoteName + "|" + chatMsg + "\n\n"
+				_, err := client.conn.Write([]byte(sendMsg))
+				if err != nil {
+					fmt.Println("conn Write err:", err)
+					break
+				}
+			}
+			chatMsg = ""
+			fmt.Println(">>>>>请输入消息，exit退出")
+			fmt.Scanln(&chatMsg)
+		}
+		client.SelectUsers()
+		fmt.Println(">>>>>>请输入聊天对象的[用户名]，exit退出")
+		fmt.Scanln(&remoteName)
+	}
 }
 
 var serverIp string
@@ -58,5 +163,8 @@ func main() {
 		return
 	}
 	fmt.Println(">>>>>>服务器连接成功")
-	select {}
+
+	go client.DealResponse()
+
+	client.Run()
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module javapub.net/go_im
 
 go 1.18
+
+require (
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4 // indirect
+	golang.org/x/vuln v0.0.0-20220725105440-4151a5aca1df // indirect
+)
